f9ebfbbd42f7f5d49bf870b80937250564168995
Victor Atavila <victoratavila@hotmail.com>
2020-12-04 09:23:30 -0300

    Version update

--- public/js/calc.js
+++ public/js/calc.js
@@ -1,17 +1,14 @@
 function cleanDiv(){
-  $('#cepFoundModal').remove();
   location.reload();
 }
-
-
 
 async function getAdress(){
 
     const cep = $('#inputCEP').val();
 
-    if(cep == null || cep == "" || cep == undefined){
-      cleanDiv();
-    } 
+    // if(cep == null || cep == "" || cep == undefined){
+    //   cleanDiv();
+    // } 
     
     await axios({
     method: 'get',
@@ -27,18 +24,33 @@ async function getAdress(){
     <div class="modal fade" id="cepFoundModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Endereço encontrado!</h5>
-            <button type="button" class="close" onclick="cleanDiv()" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true" onclick="cleanDiv()">&times;</span>
-            </button>
+          <div class="modal-header" style = "border: 0; text-align: center">
+            <h5 class="modal-title" id="staticBackdropLabel" style = "text-align: center">Endereço encontrado! <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check2" fill="green" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+          </svg></h5> 
           </div>
           <div class="modal-body">
-          <input type="text" class = "form-control border-0" style = "background-color: white" disabled value="Rua: ${adress.data.logradouro}">
-          <input type="text" class = "form-control border-0" style = "background-color: white" disabled value="Bairro: ${adress.data.bairro}">
-          <input type="text" class = "form-control border-0" style = "background-color: white" disabled value="Cidade: ${adress.data.localidade}">
-          <input type="text" class = "form-control border-0" style = "background-color: white" disabled value="Estado: ${adress.data.uf}">
-          <input type="text" class = "form-control border-0" style = "background-color: white" disabled value="DDD: (${adress.data.ddd})">
+          <table class="table table-striped" style = "text-align: center" id = "adressTable">
+          <thead>
+            <tr>
+              <th scope="col">Rua</th>
+              <th scope="col">Bairro</th>
+              <th scope="col">Cidade</th>
+              <th scope="col">Estado</th>
+              <th scope="col">DDD</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td id="street">${adress.data.logradouro}</td>
+              <td id = "neighbourhood">${adress.data.bairro}</td>
+              <td id = "city">${adress.data.localidade}</td>
+              <td id = "state">${adress.data.uf}</td>
+              <td id = "ddd">${adress.data.ddd}</td>
+            </tr>
+          </tbody>
+        </table>
+
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-primary" onclick="cleanDiv()" data-dismiss="modal">Fechar</button>
@@ -56,4 +68,9 @@ async function getAdress(){
 
 }
 
+
+function stopSubmit(event){
+  event.preventDefault();
+  getAdress();
+}
 
